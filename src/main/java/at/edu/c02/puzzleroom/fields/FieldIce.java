7e988adaaa5f449004c36162db9371cd04b985e9
kiralex-c02 <52400839@edu.campus02.at>
2025-11-14 15:47:06 +0100

    finished ice-field logic closes #2

---
+++ src/main/java/at/edu/c02/puzzleroom/fields/FieldIce.java
@@ -0,0 +1,39 @@
+package at.edu.c02.puzzleroom.fields;
+
+import at.edu.c02.puzzleroom.Direction;
+import at.edu.c02.puzzleroom.GameBoard;
+import at.edu.c02.puzzleroom.exceptions.PuzzleRoomException;
+
+public class FieldIce extends BaseField{
+    protected FieldIce(GameBoard gameBoard, char name, int row, int col) {
+        super(gameBoard, name, row, col);
+    }
+
+    @Override
+    public void initialize() throws PuzzleRoomException {
+    }
+
+    @Override
+    public boolean enterField(Direction direction) {
+        gameBoard.getPlayer().walkStep();
+        Field nextField = null;
+        switch (direction) {
+            case Up:
+                nextField = gameBoard.getField(row - 1, col);
+            case Down:
+                nextField = gameBoard.getField(row + 1, col);
+            case Left:
+                nextField = gameBoard.getField(row, col - 1);
+            case Right:
+                nextField = gameBoard.getField(row, col + 1);
+        }
+        setPlayerPositionToField();
+        nextField.enterField(direction);
+        return true;
+    }
+
+    @Override
+    public boolean leaveField(Direction direction) {
+        return false;
+    }
+}
